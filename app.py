455a18bdb73ae8d5c2f141625e6582c6a5673930
devops <94903063+confluxdevops@users.noreply.github.com>
2023-11-27 16:51:49 +0800

    Create app.py
    add main file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 记录访问次数的变量
+visit_count = 0
+
+@app.route('/')
+def index():
+    global visit_count
+    visit_count += 1
+    return render_template('index.html', count=visit_count)
+
+if __name__ == '__main__':
+    app.run(debug=True)
